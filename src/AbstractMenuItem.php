7c25a3ba229fe00738ddb03b69e5bcc6dc8fdc06
Miguel Muscat <miguelmuscat93@gmail.com>
2017-07-01 16:24:15 +0200

    Added `AbstractMenu` and `AbstractMenuItem`

---
+++ src/AbstractMenuItem.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace RebelCode\WordPress\Admin\Menu;
+
+/**
+ * Basic functionality for a menu item.
+ *
+ * @since [*next-version*]
+ */
+abstract class AbstractMenuItem extends AbstractMenuElement
+{
+    /*
+     * This trait provides the protected methods for a menu item.
+     *
+     * @since [*next-version*]
+     */
+    use MenuItemTrait;
+}
---
+++ test/functional/AbstractMenuItemTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace RebelCode\WordPress\Admin\Menu\FuncTest;
+
+use Xpmock\TestCase;
+use RebelCode\WordPress\Admin\Menu\AbstractMenuItem;
+
+/**
+ * Tests {@see RebelCode\WordPress\Admin\Menu\AbstractMenuItem}.
+ *
+ * @since [*next-version*]
+ */
+class AbstractMenuItemTest extends TestCase
+{
+    /**
+     * The classname of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'RebelCode\\WordPress\\Admin\\Menu\\AbstractMenuItem';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+            ->_onSelected()
+            ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(
+            static::TEST_SUBJECT_CLASSNAME, $subject,
+            'Subject is not a valid instance'
+        );
+
+        $this->assertInstanceOf(
+            'RebelCode\\WordPress\\Admin\\Menu\\AbstractMenuElement', $subject,
+            'Subject is not a valid instance'
+        );
+    }
+}
