f0029271c61e0fe7b182b2c279bdc14d6bcb2a84
Miguel Muscat <miguelmuscat93@gmail.com>
2017-07-04 16:31:48 +0200

    Fixed functional test for `AbstractMenuElement`

--- test/functional/AbstractMenuElementTest.php
+++ test/functional/AbstractMenuElementTest.php
@@ -2,6 +2,7 @@
 
 namespace RebelCode\WordPress\Admin\Menu\FuncTest;
 
+use Dhii\Validation\Exception\ValidationFailedException;
 use Xpmock\TestCase;
 use RebelCode\WordPress\Admin\Menu\AbstractMenuElement;
 
@@ -27,10 +28,27 @@ class AbstractMenuElementTest extends TestCase
     public function createInstance()
     {
         $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
-            ->_onSelected()
+            ->_createValidationFailedException(function($msg, $code = 0, $inner = null) {
+                return new ValidationFailedException($msg, $code, $inner);
+            })
             ->new();
 
         return $mock;
+    }
+
+    protected function createMenuElementMock()
+    {
+        $mock = $this->mock('RebelCode\\WordPress\\Admin\\Menu\\MenuElementInterface')
+            ->getKey()
+            ->getValue()
+            ->getLabel()
+            ->getCapability()
+            ->getIcon()
+            ->getParent()
+            ->getChildren()
+            ->onSelected();
+
+        return $mock->new();
     }
 
     /**
@@ -46,5 +64,71 @@ class AbstractMenuElementTest extends TestCase
             static::TEST_SUBJECT_CLASSNAME, $subject,
             'Subject is not a valid instance'
         );
+    }
+
+    public function testIsMenuElement()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+
+        $valid   = $this->createMenuElementMock();
+        $invalid = new \ArrayIterator([]);
+
+        $this->assertTrue ($reflect->_isMenuElement($valid));
+        $this->assertFalse($reflect->_isMenuElement($invalid));
+        $this->assertFalse($reflect->_isMenuElement(null));
+    }
+
+    /**
+     * Tests the parent validation method with a valid parent.
+     *
+     * @since [*next-version*]
+     */
+    public function testValidateParentValid()
+    {
+        $subject   = $this->createInstance();
+        $reflect   = $this->reflect($subject);
+        $valid     = $this->createMenuElementMock();
+        $exception = null;
+
+        try {
+            $reflect->_validateParent($valid);
+        } catch (\Exception $exception) {}
+
+        $this->assertNull($exception, 'Valid parent was not expected to throw an exception.');
+    }
+
+    /**
+ * Tests the parent validation method with an invalid parent.
+ *
+ * @since [*next-version*]
+ */
+    public function testValidateParentInvalid()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+        $invalid = new \ArrayIterator([]);
+
+        $this->setExpectedException('Dhii\\Validation\\Exception\\ValidationFailedExceptionInterface');
+
+        $reflect->_validateParent($invalid);
+    }
+
+    /**
+     * Tests the parent validation method with a null parent.
+     *
+     * @since [*next-version*]
+     */
+    public function testValidateParentNull()
+    {
+        $subject   = $this->createInstance();
+        $reflect   = $this->reflect($subject);
+        $exception = null;
+
+        try {
+            $reflect->_validateParent(null);
+        } catch (\Exception $exception) {}
+
+        $this->assertNull($exception, 'Null parent was not expected to throw an exception.');
     }
 }
